a06c0a343fc75c50d0f2b68396d6e90d2e938a62
Bo HOU <daily.bhou@gmail.com>
2016-12-14 17:53:10 +0100

    dont use webpack to package this module

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-module.exports = require("./build/prod/node/collar-transport.min.latest.js");
+//module.exports = require("./build/prod/node/collar-transport.min.latest.js");
+
+const Transport = require('./Transport');
+const LocalTransport = require('./LocalTransport');
+
+module.exports = {
+  Transport: Transport,
+  LocalTransport: LocalTransport
+}
 
